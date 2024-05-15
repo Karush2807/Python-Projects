53c0a709deaa3348ab4b1bffbd67bb5ca4715ec1
Karush2807 <arushkarnatak1881@gmail.com>
2024-05-15 16:20:25 +0530

    minor changes

--- Youtube Manager/app.py
+++ Youtube Manager/app.py
@@ -1,5 +1,35 @@
 
 
+# this will show all the videao makred fav by the user
+def all_videos(videos):
+    pass
+
+#this will tell the duration of the youtube video
+def durn_of_vid(videos):
+    pass
+
+#this will update the video details
+def updt_vid(videos):
+    pass
+
+#this will add a video to the list
+def add_vid(videos):
+    pass
+
+#this will search the video
+def search_vid(videos):
+    pass
+
+#this will delete the video
+def del_vid(videos):
+    pass
+
+#this will exit the program
+def exit():
+    pass
+
+videos=[]
+
 while True:
     print("\n Welcome To The UTube Manager!!")
     print("Choose an option from the following:")
@@ -10,3 +40,9 @@ while True:
     print("5. Search a UTube video")
     print("6. Delete a UTube video")
     print("7. Exit")
+    choice=input("Enter your choice: ")
+
+    match choice:
+        
+        case "1":
+            all_videos(videos)
