8809978fdbcfb8ecb5040e79e302525c4b847b2a
Karush2807 <arushkarnatak1881@gmail.com>
2024-05-15 16:09:26 +0530

    first commit

---
+++ Youtube Manager/app.py
@@ -0,0 +1,12 @@
+
+
+while True:
+    print("\n Welcome To The UTube Manager!!")
+    print("Choose an option from the following:")
+    print("1. List ur pasandida videos")
+    print("2. Duration of UTube videos")
+    print("3. video ki deatails update")
+    print("4. Add a utube video")
+    print("5. Search a UTube video")
+    print("6. Delete a UTube video")
+    print("7. Exit")
